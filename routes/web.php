fbebb6a6b04718f97b1a4d2067ecfb17f29d3946
Wenyw <645228905@qq.com>
2020-09-01 00:58:17 +0000

    create user valid code

--- routes/web.php
+++ routes/web.php
@@ -13,3 +13,21 @@
 
 Route::get('/', 'PagesController@root')->name('root');
 
+
+// Authentication Routes...
+$this->get('login', 'Auth\LoginController@showLoginForm')->name('login');
+$this->post('login', 'Auth\LoginController@login');
+$this->post('logout', 'Auth\LoginController@logout')->name('logout');
+
+$this->get('register', 'Auth\RegisterController@showRegistrationForm')->name('register');
+$this->post('register', 'Auth\RegisterController@register');
+
+$this->get('password/reset', 'Auth\ForgotPasswordController@showLinkRequestForm')->name('password.request');
+$this->post('password/email', 'Auth\ForgotPasswordController@sendResetLinkEmail')->name('password.email');
+$this->get('password/reset/{token}', 'Auth\ResetPasswordController@showResetForm')->name('password.reset');
+$this->post('password/reset', 'Auth\ResetPasswordController@reset')->name('password.update');
+
+$this->get('email/verify', 'Auth\VerificationController@show')->name('verification.notice');
+$this->get('email/verify/{id}', 'Auth\VerificationController@verify')->name('verification.verify');
+$this->get('email/resend', 'Auth\VerificationController@resend')->name('verification.resend');
+
