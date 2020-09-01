9a5dfe4b2144b2a61ee9b80214be97e909f5d6fc
Wenyw <645228905@qq.com>
2020-09-01 07:24:24 +0000

    edit myinfo

--- resources/views/layouts/_header.blade.php
+++ resources/views/layouts/_header.blade.php
@@ -25,14 +25,8 @@
           </a>
           <div class="dropdown-menu" aria-labelledby="navbarDropdown">
             <a class="dropdown-item" href="#">
-              <i class="far fa-user mr-2"></i>
-              个人中心
-            </a>
-            <div class="dropdown-divider"></div>
-            <a class="dropdown-item" href="#">
-              <i class="far fa-edit mr-2"></i>
-              编辑资料
-            </a>
+              <a class="dropdown-item" href="{{ route('users.show', Auth::id()) }}">个人中心</a>
+              <a class="dropdown-item" href="{{ route('users.edit', Auth::id()) }}">编辑资料</a>
             <div class="dropdown-divider"></div>
             <a class="dropdown-item" id="logout" href="#">
               {{--<form action="{{ route('logout') }}" method="POST" onsubmit="return confirm('您确定要退出吗？');">--}}
