a730d3350f71f8685385fbbecf7385d17ee1fa9c
Wenyw <645228905@qq.com>
2020-09-02 01:55:12 +0000

    category list

--- routes/web.php
+++ routes/web.php
@@ -33,6 +33,6 @@ $this->get('email/resend', 'Auth\VerificationController@resend')->name('verifica
 
 Route::resource('users', 'UsersController', ['only' => ['show', 'update', 'edit']]);
 
-
-
 Route::resource('topics', 'TopicsController', ['only' => ['index', 'show', 'create', 'store', 'update', 'edit', 'destroy']]);
+
+Route::resource('categories', 'CategoriesController', ['only' => ['show']]);
