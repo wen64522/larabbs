3fc5f6d7ad43590bf419e99946903c2ae81ea1a4
Wenyw <645228905@qq.com>
2020-09-04 03:22:08 +0000

    user active time

--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -9,13 +9,13 @@
     <div class="col-lg-3 col-md-3 hidden-sm hidden-xs user-info">
       <div class="card ">
         <img class="card-img-top" src="{{ $user->avatar }}" alt="{{ $user->name }}">
-        {{--<div class="card-body">--}}
-          {{--<h5><strong>注册于</strong></h5>--}}
-          {{--<p>{{ $user->created_at->diffForHumans() }}</p>--}}
-          {{--<hr>--}}
-          {{--<h5><strong>最后活跃</strong></h5>--}}
-          {{--<p title="{{  $user->last_actived_at }}">{{ $user->last_actived_at->diffForHumans() }}</p>--}}
-        {{--</div>--}}
+        <div class="card-body">
+          <h5><strong>注册于</strong></h5>
+          <p>{{ $user->created_at->diffForHumans() }}</p>
+          <hr>
+          <h5><strong>最后活跃</strong></h5>
+          <p title="{{  $user->last_actived_at }}">{{ $user->last_actived_at->diffForHumans() }}</p>
+        </div>
         <div class="card-body">
           <h5><strong>个人简介</strong></h5>
           <p>{{ $user->introduction }}</p>
