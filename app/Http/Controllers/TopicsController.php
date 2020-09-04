295349b05bf480bd5f5c965004cc3859b8e40b69
Wenyw <645228905@qq.com>
2020-09-04 01:55:42 +0000

    active user

--- app/Http/Controllers/TopicsController.php
+++ app/Http/Controllers/TopicsController.php
@@ -9,7 +9,7 @@ use App\Http\Controllers\Controller;
 use App\Http\Requests\TopicRequest;
 use App\Models\Category;
 use Illuminate\Support\Facades\Auth;
-
+use App\Models\User;
 class TopicsController extends Controller
 {
 	public function __construct()
@@ -17,10 +17,11 @@ class TopicsController extends Controller
 		$this->middleware('auth', ['except' => ['index', 'show']]);
 	}
 
-	public function index(Request $request, Topic $topic)
+	public function index(Request $request, Topic $topic,User $user)
 	{
 		$topics = $topic->withOrder($request->order)->paginate(20);
-		return view('topics.index', compact('topics'));
+		$active_users = $user->getActiveUsers();
+		return view('topics.index', compact('topics','active_users'));
 	}
 
     public function show(Request $request, Topic $topic)
